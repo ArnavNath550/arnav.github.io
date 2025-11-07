a7efb89c696d20a73868ca81960e9768c653f5cf
Arnav Nath <arnavnath550@gmail.com>
2025-11-08 04:38:18 +1300

    ts errors fixed - todoComponent -v2

--- src/components/ShowComponents/TodoComponent.tsx
+++ src/components/ShowComponents/TodoComponent.tsx
@@ -112,7 +112,7 @@ const TodoComponent: React.FC = () => {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
+      transition: { duration: 0.6, ease: [0.4, 0, 0.2, 1] },
     },
   };
 
